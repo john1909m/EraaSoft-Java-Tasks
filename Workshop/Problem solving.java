dcb0270a37c020530814a6862399f968ca85c754
john1909m <johnemil21@yahoo.com>
2025-10-25 20:54:29 +0300

    adding what we have done in the workshop

---
+++ Workshop/Problem solving.java
@@ -0,0 +1,150 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        //---------------------------------Update range-------------------//////////////
+
+//        Scanner scanner = new Scanner(System.in);
+//        int n=scanner.nextInt();
+//        int q=scanner.nextInt();
+//        int[] array=new int[n];
+//        for(int i=0;i<n;i++){
+//            array[i]=scanner.nextInt();
+//        }
+//
+//        for(int i=0;i<q;i++){
+//            int num1=scanner.nextInt();
+//            int num2=scanner.nextInt();
+//            if(num2>n){
+//                num2=array.length-1;
+//            }
+//            int value=scanner.nextInt();
+//            for(int j=num1;j<=num2;j++){
+//                array[j-1]=array[j-1]+value;
+//            }
+//        }
+//
+//        System.out.println(Arrays.toString(array));
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        //------------------------count vowels----------------------//
+
+
+//        String text=scanner.nextLine();
+//        char[] vowels={'a','e', 'i', 'o', 'u'};
+//        int count=0;
+//        text=text.toLowerCase();
+//        for(int i=0;i<text.length();i++){
+//            for(int j=0;j<vowels.length;j++){
+//                if(text.charAt(i)==vowels[j]){
+//                    count++;
+//                }
+//            }
+//        }
+//        System.out.println(count);
+
+
+
+        //----------------------------mirror array-----------------------//
+
+//    int n=scanner.nextInt();
+//    int m=scanner.nextInt();
+//    int[][] array=new int[n][m];
+//    for(int i=0;i<n;i++){
+//
+//        for(int j=0;j<m;j++){
+//            array[i][j]=scanner.nextInt();
+//        }
+//    }
+//
+//
+//   for(int i=0;i<n;i++){
+//        for(int j=m-1;j>=0;j--){
+//            System.out.print(array[i][j]+" ");
+//        }
+//        System.out.print("\n");
+//    }
+
+
+        //-----------------The last 2 digits---------//
+
+//    int a = scanner.nextInt();
+//    int b = scanner.nextInt();
+//    int c = scanner.nextInt();
+//    int d = scanner.nextInt();
+//    int ans=a*b*c*d;
+//    String answer=String.valueOf(ans%100);
+//    System.out.println(answer);
+
+//----------Shape3--------------//
+//    int input = scanner.nextInt();
+//
+//    int space = input-1;
+//    int a = 1;
+//
+//    for (int i = 1; i <= input; i++) {
+//
+//        for (int j = 1; j <= space; j++) {
+//            System.out.print(" ");
+//        }
+//
+//        for (int k = 1; k <= a; k++) {
+//            System.out.print("*");
+//        }
+//        System.out.println();
+//        space -= 1;
+//        a +=2;
+//    }
+//    a-=2;
+//    space -=1;
+//
+//
+//    for (int i = 1; i <= input; i++) {
+//        for (int j = 1; j <= space; j++) {
+//            System.out.println(" ");
+//        }
+//        for (int k = 1; k <= a; k++) {
+//            System.out.print("*");
+//        }
+//        System.out.println();
+//        space += 1;
+//        a -=2;
+//    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////        int[] query;
+//        for(int i=0;i<q;i++){
+//             int
+//        }
+//        for(int j=0;j<q;j++){
+//
+//        }
+    }
+}
